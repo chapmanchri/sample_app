bb220514c4510aa618250450e74834d0e2deaa6c
Chris Chapman <chapmanchri@gmail.com>
2014-08-17 22:35:53 -0400

    Updatead application to use postgres

--- spec/requests/static_pages_spec2.rb
+++ spec/requests/static_pages_spec2.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
+
+
 describe "Static pages" do
+  
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   
   describe "Home page" do
 
@@ -11,7 +17,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -26,7 +32,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
   end 
@@ -40,7 +46,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
 
   end 
@@ -54,7 +60,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
 
   end 
